c3246e2ebb8cd85b86950bebb1109518bb873609
Rifqi Dani Putranto <rifqidani233@gmail.com>
2024-09-17 21:59:39 +0700

    array null

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,37 +4,10 @@ package com.dicoding.kotlin
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
     fun main() {
-        val text  = "teresa"
-        for (char in text){
-            //string template
-            print("$char ")
-
-
+        val mixArray: Array<Any?> = arrayOfNulls(5)
+        println(mixArray.size)
+        for(i in 0 until mixArray.size){
+            println(mixArray[i])
         }
-
-        //Escaped String
-        val statement = "Kotlin is \"Awesome!\""
-        println("\nstatement")
-
-        //unicode
-        val unicode = "Unicode test: \u00A9"
-        println(unicode)
-
-        //RawString
-        val line = """
-        Line 1
-        Line 2
-        Line 3
-        Line 4
-    """.trimIndent()
-
-        print(line)
-
-
-
     }
-
-    /*
-      output : The 4th character of the Dicoding is o
-    */
 
