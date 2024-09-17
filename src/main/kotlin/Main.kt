17de58006eef07d8534603f67c3924e39643cc9c
Rifqi Dani Putranto <rifqidani233@gmail.com>
2024-09-17 21:31:20 +0700

    variabel

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,8 +6,32 @@ package com.dicoding.kotlin
     fun main() {
         val text  = "teresa"
         for (char in text){
+            //string template
             print("$char ")
+
+
         }
+
+        //Escaped String
+        val statement = "Kotlin is \"Awesome!\""
+        println("\nstatement")
+
+        //unicode
+        val unicode = "Unicode test: \u00A9"
+        println(unicode)
+
+        //RawString
+        val line = """
+        Line 1
+        Line 2
+        Line 3
+        Line 4
+    """.trimIndent()
+
+        print(line)
+
+
+
     }
 
     /*
