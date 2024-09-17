9da9f5327e70c36154df56087ff7db3bfdbf096c
Rifqi Dani Putranto <rifqidani233@gmail.com>
2024-09-17 21:16:55 +0700

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+package com.dicoding.kotlin
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+    fun main() {
+        val text  = "teresa"
+        for (char in text){
+            print("$char ")s
+        }
+    }
+
+    /*
+      output : The 4th character of the Dicoding is o
+    */
+
